eb6d505e8984e66593d2709e263ad5d3ecb28457
carlosttorres33 <kisamehatake33@comunidad.unam.mx>
2022-09-24 15:08:32 -0500

    Implementacion de la Splash Screen

--- app/src/main/java/com/example/quepeli/ui/MainActivity.kt
+++ app/src/main/java/com/example/quepeli/ui/MainActivity.kt
@@ -6,7 +6,10 @@ import com.example.quepeli.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        Thread.sleep(3000)
+        setTheme(R.style.Theme_QuePeli)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
