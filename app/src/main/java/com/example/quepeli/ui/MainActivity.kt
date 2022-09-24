c3bd6489112e70d7db54aa3ae0d56d2d7cb93d66
carlosttorres33 <kisamehatake33@comunidad.unam.mx>
2022-09-24 13:27:50 -0500

    Actualización de diseño

--- app/src/main/java/com/example/quepeli/ui/MainActivity.kt
+++ app/src/main/java/com/example/quepeli/ui/MainActivity.kt
@@ -6,8 +6,6 @@ import com.example.quepeli.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        Thread.sleep(3000)
-        setTheme(R.style.Theme_QuePeli)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
